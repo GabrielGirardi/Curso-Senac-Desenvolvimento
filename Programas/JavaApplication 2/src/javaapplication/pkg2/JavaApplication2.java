31f73b7587851414cbe061ddc151c6f31aff0e17
GABRIEL GIRARDI <gabriel.girardi@alunos.sc.senac.br>
2021-08-09 21:50:20 -0300

    Update em Programas

--- Programas/JavaApplication 2/src/javaapplication/pkg2/JavaApplication2.java
+++ Programas/JavaApplication 2/src/javaapplication/pkg2/JavaApplication2.java
@@ -14,49 +14,49 @@ import java.util.Scanner;
 public class JavaApplication2 {
 
     public static void main(String[] args) {
-               float a = 0;
-               float c = 0;
-               float d = 0;
-                 Scanner input = new Scanner(System.in);
-                  System.out.println("informe um valor: ");
-                     a = input.nextInt();                      
+        float a = 0;
+        float c = 0;
+        float d = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("informe o primeiro valor: ");
+        a = input.nextInt();                               
                        
-                       System.out.println("Informe a operação (1 +, 2 -, 3 *, 4 /  ");
-                       int b = input.nextInt();
-                       
-
-                       switch (b)
-                       {
+        System.out.println("Informe a operacao (1 +, 2 -, 3 *, 4 /  ");
+        int b = input.nextInt();
+              
+        
+                   switch (b)
+                   {
                          case 1:
-                               System.out.println("Informação inserida é (+), digite o proximo valor: ");
+                               System.out.println("Informacao inserida e (+), digite o proximo valor: ");
                                c = input.nextInt ();
                                d = a + c;
-                               System.out.println(a + " + " + b + " = " + d);
+                               System.out.println(a + " + " + c + " = " + d);
                                break;
                                
                                
                          case 2: 
-                               System.out.println("Informação inserida é (-), digite o proximo valor: ");
+                               System.out.println("Informacao inserida e (-), digite o proximo valor: ");
                                c = input.nextInt ();
                                d = a - c;
-                               System.out.println(a + " - " + b + " = " + d);
+                               System.out.println(a + " - " + c + " = " + d);
                                break;
                                
                          case 3:
-                               System.out.println("Informação inserida é (*), digite o proximo valor: * ");
+                               System.out.println("Informacao inserida e (*), digite o proximo valor: ");
                                c = input.nextInt ();
                                d = a * c;
-                               System.out.println(a + " * " + b + " = " + d);
+                               System.out.println(a + " * " + c + " = " + d);
                                break;
                                
                          case 4: 
-                               System.out.println("Informação inserida é (/),  digite o proximo valor: ");
+                               System.out.println("Informacao inserida e (/),  digite o proximo valor: ");
                                c = input.nextInt ();
                                d = a / c;
-                               System.out.println(a + " / " + b + " = " + d);
+                               System.out.println(a + " / " + c + " = " + d);
                                break;
                        
-             }
+       }
                              
     }
     
