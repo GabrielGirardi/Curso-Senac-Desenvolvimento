d096fa28e57871a3c09ed6512b1a8cb412cd863b
GABRIEL GIRARDI <gabriel.girardi@alunos.sc.senac.br>
2021-08-16 22:03:09 -0300

    upload 100% logistic

--- Programas/Logistic/src/logistic/Logistic.java
+++ Programas/Logistic/src/logistic/Logistic.java
@@ -17,26 +17,26 @@ public class Logistic {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int[] ListNum = new int[20];
-    int a = 0;
-    int resp = 0;
+        Scanner input = new Scanner(System.in);
+        int[] ListNum = new int[20];
+        int a = 0;
+        int resp = 0;
     String i = "0";
     System.out.println("Por favor, insira o valor alvo: ");
     resp = input.nextInt();
     while (!i.equals("n")) {
-      System.out.println("Informe um valor da lista: ");
-      ListNum[a] = input.nextInt();
-      System.out.println("Deseja continuar? (Y/n)");
-      i = input.next();
-      a++;
-    } 
-    System.out.println("Finalizado");
-    for (int c = 0; c < ListNum.length; c++) {
-      for (int d = 0; d < ListNum.length; d++) {
-        if (ListNum[c] != 0 && ListNum[d] != 0 && ListNum[c] + ListNum[d] == resp) {
-          System.out.print("" + ListNum[c] + " + " + ListNum[c] + " = ");
-          System.out.println(ListNum[c] + ListNum[d]);
+        System.out.println("Informe um valor da lista: ");
+        ListNum[a] = input.nextInt();
+        System.out.println("Deseja continuar? (Y/n)");
+        i = input.next();
+        a++;
+    }
+        System.out.println("Finalizado");
+            for (int c = 0; c < ListNum.length; c++) {
+                for (int d = 0; d < ListNum.length; d++) {
+                    if (ListNum[c] != 0 && ListNum[d] != 0 && ListNum[c] + ListNum[d] == resp) {
+                         System.out.print("" + ListNum[c] + " + " + ListNum[d] + " = ");
+                               System.out.println(ListNum[c] + ListNum[d]);
         } 
       } 
     } 
