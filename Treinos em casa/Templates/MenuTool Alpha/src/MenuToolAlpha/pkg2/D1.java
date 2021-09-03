bc52a869ae9e6ddef124b2dddfd49b781beb859b
Gabriel Girardi <1014899@UNIDAVI.local>
2021-09-03 15:32:08 -0300

    upload e criação de pastas 03/09/21

---
+++ Treinos em casa/Templates/MenuTool Alpha/src/MenuToolAlpha/pkg2/D1.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MenuToolAlpha.pkg2;
+
+import java.util.*;
+/**
+ *
+ * @author gabriel.girardi
+ */
+public class D1 implements Function{
+        
+    private String A1;
+    private String A2;
+    private String A3;   
+    private String A4;
+    private String A5;
+    private String A6;
+
+    D1(String a1, String a2, String a3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public boolean equals(D1 d1)
+       {
+        return(this.A1.equals(d1.A1) && this.A2.equals(d1.A2) && this.A3.equals(d1.A3)&& this.A4.equals(d1.A4)&& this.A5.equals(d1.A5)&& this.A6.equals(d1.A6));
+        }
+        
+    public String getA1() {
+        return A1;
+    }
+
+    public void setA1(String A1) {
+        this.A1 = A1;
+    }
+
+    public String getA2() {
+        return A2;
+    }
+
+    public void setA2(String A2) {
+        this.A2 = A2;
+    }
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
+    }
+     public String getA4() {
+        return A4;
+    }
+
+    public void setA4(String A4) {
+        this.A4 = A4;
+    }
+        public String getA5() {
+        return A5;
+    }
+
+    public void setA5(String A5) {
+        this.A5 = A5;
+    }
+        public String getA6() {
+        return A6;
+    }
+
+    public void setA6(String A6) {
+        this.A6 = A6;
+    }
+    public D1(String A1, String A2, String A3, String A4, String A5, String A6) {
+      
+      this.A1 = A1;
+       this.A2 = A2;
+        this.A3 = A3;
+         this.A4 = A3;
+          this.A5 = A3;
+           this.A6 = A3;
+    }
+
+    @Override
+    public void cadDados() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void lerDados() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
---
+++ Treinos em casa/Templates/MenuTool Alpha/src/MenuToolAlpha/pkg2/Function.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MenuToolAlpha.pkg2;
+
+/**
+ *
+ * @author gabriel.girardi
+ */
+public interface Function {
+    public abstract void cadDados();
+    public abstract void lerDados();
+}
